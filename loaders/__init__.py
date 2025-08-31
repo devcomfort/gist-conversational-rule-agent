6582b885d8e48289e036f1d15c33e1a22b4f2c14
devcomfort <im@devcomfort.me>
2025-09-01 05:23:11 +0900

    feat(loaders): collect_document_paths 공개 API로 export 추가

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -3,6 +3,7 @@ from .load_docx import load_docx
 from .load_hwp import load_hwp
 from .load_pdf import load_pdf
 from .load_pptx import load_pptx
+from .collect_document_paths import collect_document_paths
 
 __all__ = [
     "load_docx",
@@ -10,4 +11,5 @@ __all__ = [
     "load_pdf",
     "load_pptx",
     "load_document",
+    "collect_document_paths",
 ]
